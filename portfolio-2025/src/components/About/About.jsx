bb1a1b0cb41257112d61cef91b4d6f9a86cefad0
yeexuean <yeexuean0502@gmail.com>
2025-02-23 02:52:32 +0000

    about me intro

---
+++ portfolio-2025/src/components/About/About.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./About.module.css";
+
+export const About = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1>
+          hello! <span>I'm Yee Xuean</span>
+        </h1>
+        <p className={styles.about}>
+          I am a second year student studying Interactive Media and Web
+          Technologies, where I combine design and development to create
+          intuitive digital experiences. I enjoy building responsive,
+          interactive websites that focus on both functionality and aesthetics.
+          Always eager to learn and improve, I stay up to date with web trends
+          and enjoy refining my skills through hands-on projects.
+        </p>
+        <div className={styles.blur} />
+      </div>
+    </section>
+  );
+};
