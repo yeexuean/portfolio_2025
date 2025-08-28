98e6250c2dc6f5b9c14f910d2e436da438db3985
yeexuean <yeexuean0502@gmail.com>
2025-08-28 18:03:11 +0800

    responsive layout
    made 3 diff responsive layouts 1024, 768, 430

--- portfolio-2025/src/components/About/About.jsx
+++ portfolio-2025/src/components/About/About.jsx
@@ -5,15 +5,17 @@ export const About = () => {
     <section className={styles.container} id="about">
       <div className={styles.content}>
         <h1>
-          hello! <span>I&apos;m Yee Xuean</span>
+          hello! <span> I&apos;m Yee Xuean</span>
         </h1>
         <p className={styles.about}>
-          I am a second year student studying Interactive Media and Web
-          Technologies, where I combine design and development to create
-          intuitive digital experiences. I enjoy building responsive,
-          interactive websites that focus on both functionality and aesthetics.
-          Always eager to learn and improve, I stay up to date with web trends
-          and enjoy refining my skills through hands-on projects.
+          I am a second year student studying
+          <span>Interactive Media and Web Technologies</span>, where I combine
+          design and development to
+          <span>create intuitive digital experiences.</span> I enjoy building
+          responsive, interactive websites that focus on both{" "}
+          <span>functionality and aesthetics.</span> Always eager to learn and
+          improve, I stay up to date with web trends and enjoy refining my
+          skills through hands-on projects.
         </p>
         <div className={styles.blur} />
       </div>
